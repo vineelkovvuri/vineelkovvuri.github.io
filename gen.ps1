7f12f845bf8f84d279216fce5b1532efda132daf
Vineel Kumar Reddy Kovvuri <vineel.kovvuri@gmail.com>
2018-12-10 06:49:15 -0800

    Fixups
    Signed-off-by: Vineel Kumar Reddy Kovvuri <vineel.kovvuri@gmail.com>

--- gen.ps1
+++ gen.ps1
@@ -5,27 +5,23 @@ Function Generate-HTML
 
     param ($InFile)
 
-$HtmlHeader = "
-<!DOCTYPE html>
+$HtmlHeader = "<!DOCTYPE html>
 <html>
     <body>
         <pre>
-
 ";
     $Lines = (Get-Content $InFile -Encoding UTF8);
     # If it appears like a tag we replace the < and > with &lt; and &gt;
     $Lines = $Lines | % {$_ -replace '<(.*?)>', '&lt$1&gt;'}
     # Words which look like beginning of the tag we replace '<word' with '< word'
-    $Lines = $Lines | % {$_ -replace '<([a-zA-Z]*?)', '< $1'}
+    $Lines = $Lines | % {$_ -replace '<([a-zA-Z]+?)', '< $1'}
     $InFileContent = $Lines -join "`r`n";
     $InFileContent = $InFileContent -replace 'img:{(.*?)}', '<img src="$1"/>'
 
 $HtmlFooter = "
-
         </pre>
     </body>
-</html>
-";
+</html>";
 
     $OutFile = $InFile -replace ".txt$", ".html"
     Set-Content -Encoding UTF8 -Path $OutFile -Value "$HtmlHeader$InFileContent$HtmlFooter"
